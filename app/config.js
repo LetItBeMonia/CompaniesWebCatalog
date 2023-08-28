31ac91276480c35a0e94985b122d6ae01f71931c
LetItBeMonia <89008855+LetItBeMonia@users.noreply.github.com>
2023-08-28 11:17:17 +0200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8888,
     databaseURL: process.env.DATABASE || 'mongodb://localhost:27017/node-kurs',
     email: 'mojemail@kontakt.pl',
     sessionKeySecret: process.env.SESSION_KEY_SECRET
