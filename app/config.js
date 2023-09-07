448d6be2d1d4a4ba03420dc9457417aa8750654f
Monika Kulig <monika.kulig.99@gmail.com>
2023-09-07 23:51:53 +0200

    fixes

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 8888,
-    databaseURL: process.env.DATABASE || 'mongodb://localhost:27017/node-kurs',
+    databaseURL: process.env.DATABASE,
     email: 'mojemail@kontakt.pl',
     sessionKeySecret: process.env.SESSION_KEY_SECRET
 };
